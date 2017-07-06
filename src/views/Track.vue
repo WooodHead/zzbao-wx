01d6beef22e60a3e167af2231b57ce3ae0c0f1d3
swimly <979741120@qq.com>
2017-07-06 10:57:19 +0800

    修改订单跟踪样式

--- src/views/Track.vue
+++ src/views/Track.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="page gray">
-    <timeline style="background:#fff;">
+    <timeline style="background:#fff;padding:1rem 1rem 0 2rem;">
       <timeline-item v-for="(item, index) in list" :key="index">
           <div class="row w">
-            <span class="col v-m">{{item.text}}</span><span class="col v-m t-r">{{item.time}}</span>
+            <span class="col v-m col-12">{{item.text}}</span><span class="col v-m t-r col-12">{{item.time}}</span>
           </div>
       </timeline-item>
     </timeline>
@@ -58,9 +58,15 @@
   }
 </script>
 <style>
-.vux-timeline-item:first-child .vux-timeline-item-content{padding-top:0.5rem !important;}
+.vux-timeline-item .vux-timeline-item-content{padding-top:0.5rem !important;}
 .vux-timeline-item-content{padding:0 1rem 3rem 2rem !important;}
 .vux-timeline-item-content .col{font-size:1rem;color:#707070;}
 .vux-timeline-item-content .col:first-child{color:#333;}
+.vux-timeline-item-tail{background:#E6E6E6 !important;}
+.weui-icon-success-no-circle{display:inline-block !important;}
+.vux-timeline-item-head{width:20px !important;height:20px !important;left:-4px !important;top:5px !important;}
+.vux-timeline-item:last-child .vux-timeline-item-checked.weui-icon-success-no-circle{font-family: "iconfont";background:#fff;border:1px solid #E1E1E1;border-radius:50%;}
+.vux-timeline-item:Last-child .vux-timeline-item-color{background:#fff !important;}
+.vux-timeline-item:last-child .vux-timeline-item-checked.weui-icon-success-no-circle::before{content:"\e61a";color:#E1E1E1;top:50%;left:50%;transform:translate(-52%,-52%);font-size:1.5rem;}
 </style>
 
