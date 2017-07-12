d8e0a1768b803789cdd20a51f8dde838efdcd17a
swimly <979741120@qq.com>
2017-07-12 11:30:25 +0800

    支付详情

--- src/views/PayInfo.vue
+++ src/views/PayInfo.vue
@@ -1,24 +1,24 @@
 <template>
   <div class="page gray">
-    <group gutter="0">
-      <cell title="投保公司" value="太平洋保险"></cell>
+    <group gutter="0" v-if="order.companyName">
+      <cell title="投保公司" :value="order.companyName"></cell>
       <cell title="保单信息" value="保单详情" is-link :link="'/policy/' + form.userId + '/' + form.orderId"></cell>
-      <cell title="商业险" value="￥2837.50"></cell>
-      <cell title="交强险" value="￥760.00"></cell>
-      <cell title="车船税" value="￥360.00"></cell>
+      <cell title="商业险" :value="'￥' + order.extraAmount"></cell>
+      <cell title="交强险（车船费）" :value="'￥' + order.baseAmount"></cell>
       <cell>
-        <p slot="value">应付总额：<span class="num c-red">￥4832.00</span></p>
+        <p slot="value">应付总额：<span class="num c-red">￥{{order.amount}}</span></p>
       </cell>
     </group>
     <group gutter="10px">
-      <cell title="订单号：" value="faw452452145214" value-align="left"></cell>
-      <cell title="支付方式：" value="支付宝" value-align="left"></cell>
-      <cell title="支付金额：" value="￥2545.00" value-align="left"></cell>
+      <cell title="订单号：" :value="order.orderSn" value-align="left"></cell>
+      <cell title="支付方式：" value="微信" value-align="left"></cell>
+      <cell title="支付金额：" :value="'￥' + order.amount" value-align="left"></cell>
     </group>
   </div>
 </template>
 <script>
   import {Group, Cell} from 'vux'
+  import {orderInfo} from '../config'
   export default {
     name: 'payInfo',
     components: {
@@ -27,10 +27,28 @@
     },
     data () {
       return {
+        order: {},
         form: {
           userId: this.$route.params.userId,
           orderId: this.$route.params.orderId
         }
+      }
+    },
+    created () {
+      this.getDetail()
+    },
+    methods: {
+      getDetail () {
+        this.$http({
+          method: 'jsonp',
+          url: orderInfo,
+          jsonp: 'callback',
+          jsonpCallback: 'json',
+          params: this.form
+        })
+        .then(res => {
+          this.order = res.body.data.order
+        })
       }
     }
   }
