e363d3c808f041814f2bc9a77bd6ca70d2c7ba01
swimly <979741120@qq.com>
2017-08-01 00:43:28 +0800

    添加支付成功ios跳转

--- src/views/PaySuccess.vue
+++ src/views/PaySuccess.vue
@@ -5,24 +5,24 @@
         <h2 class="tip"><span class="iconfont icon-correct"></span>订单支付成功</h2>
         <ul class="row w" style="margin-top:1rem;">
           <li class="col v-m col-12" style="padding:0 0.5rem 0 4rem;">
-            <x-button plain type="primary" class="custom-primary-red" @click.native="jump('/orderdetail/' + userId + '/' + orderId + '/web')">查看订单</x-button>
+            <x-button plain type="primary" class="custom-primary-red" @click.native="seeOrder">查看订单</x-button>
           </li>
           <li class="col v-m col-12" style="padding:0 4rem 0 0.5rem;">
-            <x-button plain type="primary" class="custom-primary-red" @click.native="jump('/')">返回首页</x-button>
+            <x-button plain type="primary" class="custom-primary-red" @click.native="backHome">返回首页</x-button>
           </li>
         </ul>
       </div>
     </group>
     <group gutter="10px">
       <cell title="订单号：" :value="info.orderSn" value-align="left"></cell>
-      <cell title="支付方式：" value="微信" value-align="left"></cell>
+      <cell title="支付方式：" :value="payType" value-align="left"></cell>
       <cell title="支付金额：" :value="'￥' + info.amount" value-align="left"></cell>
     </group>
   </div>
 </template>
 <script>
   import {Group, Cell, XButton} from 'vux'
-  import {orderInfo} from '../config'
+  import {orderInfo, server} from '../config'
   export default {
     head: {
       title: {
@@ -50,6 +50,24 @@
       this.getDetail()
     },
     methods: {
+      seeOrder () {
+        if (this.$route.query.platform === 'ios') {
+          loadURL(server + '/?showOrder=click')
+        } else if (this.$route.query.platform === 'android') {
+          jsToApp.back()
+        } else {
+          this.jump('/orderdetail/' + this.userId + '/' + this.orderId + '/web')
+        }
+      },
+      backHome () {
+        if (this.$route.query.platform === 'ios') {
+          loadURL(server + '/?back=home')
+        } else if (this.$route.query.platform === 'android') {
+          jsToApp.back()
+        } else {
+          this.jump('/')
+        }
+      },
       jump (url) {
         this.$router.push(url)
       },
@@ -67,6 +85,20 @@
         .then(res => {
           console.log(res)
           this.info = res.body.data.order
+          switch (this.info.payType) {
+            case 0:
+              this.payType = '未知'
+              break
+            case 1:
+              this.payType = '支付宝APP'
+              break
+            case 2 :
+              this.payType = '微信APP'
+              break
+            case 3 :
+              this.payType = '微信公众号'
+              break
+          }
         })
       }
     }
